790a1e2304b96a0813964b7186adaf821299285b
ZqlwMatt <2268236614@qq.com>
2024-10-08 08:20:27 +0800

    chore(bullet): add twoShoot mode.

--- assets/Scripts/Player.ts
+++ assets/Scripts/Player.ts
@@ -1,17 +1,35 @@
 import { _decorator, Component, EventTouch, Input, input, instantiate, Node, Prefab, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
 
+enum ShootType {
+    OneShoot,
+    TwoShoot
+};
+
 @ccclass('Player')
 export class Player extends Component {
     
     @property
     shootRate:number = 0.3;
+
     @property(Node)
     bulletParent:Node = null;
+
+    // oneShoot assets
     @property(Prefab)
     bullet1Prefab:Prefab = null;
     @property(Node)
-    bullet1Position:Node = null;
+    position1:Node = null;
+    // twoShoot assets
+    @property(Prefab)
+    bullet2Prefab:Prefab = null;
+    @property(Node)
+    position2:Node = null;
+    @property(Node)
+    position3:Node = null;
+
+    @property
+    shootType:ShootType = ShootType.OneShoot;
 
     shootTimer:number = 0.0;
 
@@ -47,13 +65,38 @@ export class Player extends Component {
     }
 
     protected update(deltaTime: number) {
+        switch(this.shootType) {
+            case ShootType.OneShoot:
+                this.oneShoot(deltaTime);
+                break;
+            case ShootType.TwoShoot:
+                this.twoShoot(deltaTime);
+                break;
+        }
+    }
+
+    oneShoot(deltaTime: number) {
         this.shootTimer += deltaTime;
         if (this.shootTimer >= this.shootRate) {
             this.shootTimer = 0;
             const bullet1 = instantiate(this.bullet1Prefab);
             // 把子弹放到场景当中
             this.bulletParent.addChild(bullet1);
-            bullet1.setWorldPosition(this.bullet1Position.worldPosition);
+            bullet1.setWorldPosition(this.position1.worldPosition);
+        }
+    }
+
+    twoShoot(deltaTime: number) {
+        this.shootTimer += deltaTime;
+        if (this.shootTimer >= this.shootRate) {
+            this.shootTimer = 0;
+            const bullet1 = instantiate(this.bullet2Prefab);
+            const bullet2 = instantiate(this.bullet2Prefab);
+            // 把子弹放到场景当中
+            this.bulletParent.addChild(bullet1);
+            this.bulletParent.addChild(bullet2);
+            bullet1.setWorldPosition(this.position2.worldPosition);
+            bullet2.setWorldPosition(this.position3.worldPosition);
         }
     }
 }
